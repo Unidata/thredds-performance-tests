190486a5811a9423dc8b0b026614aeaf4b29b729
Tara Drwenski <tdrwenski@ucar.edu>
2023-09-01 13:22:20 -0600

    Give both containers the same name

--- run-all.sh
+++ run-all.sh
@@ -4,7 +4,7 @@ cd tds/
 docker build -t thredds-performance-tests:5.5-SNAPSHOT .
 docker-compose up -d tds-no-caching
 
-until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests-no-caching\
+until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
 | grep -m 1 "healthy"; do sleep 1 ; done
 
 cd ../tests/
