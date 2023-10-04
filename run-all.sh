b560fbf6fcb87cc206bd8e49d54f782986a6d5df
Tara Drwenski <tdrwenski@ucar.edu>
2023-10-04 15:26:23 -0600

    Fix health check to not match "unhealthy"

--- run-all.sh
+++ run-all.sh
@@ -5,7 +5,7 @@ docker build -t thredds-performance-tests:5.5-SNAPSHOT .
 docker compose up -d tds-no-caching
 
 until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests-no-caching\
-| grep -m 1 "healthy"; do sleep 1 ; done
+| grep -m 1 '"healthy"'; do sleep 1 ; done
 
 cd ../tests/
 docker build -t performance-tests:latest .
