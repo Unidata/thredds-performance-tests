e2e1b373951645f88e6698dcbb930bb7af421885
Tara Drwenski <tdrwenski@ucar.edu>
2023-10-16 10:02:51 -0600

    Improve run script Create version folder if does not exist and add comments to script

--- run-all.sh
+++ run-all.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
+# start tds
 cd tds/
 docker build -t thredds-performance-tests:5.5-SNAPSHOT .
 docker compose up -d tds-no-caching
-
-
 until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
 | grep -m 1 '"healthy"'; do sleep 1 ; done
+
+# get tds version info
+mkdir -p ../tests/version/
 docker cp thredds-performance-tests:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
 
+# run tests
 cd ../tests/
 mkdir -p results/
 docker build -t performance-tests:latest .
 docker run --rm --network="host" -v ./results/:/usr/src/app/results/ --user $(id -u):$(id -g) performance-tests
 
+# stop tds
 cd ../tds/
 docker compose down
