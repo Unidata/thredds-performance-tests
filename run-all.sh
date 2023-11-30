9c8f1c13db72a69fd53fefc7ebde34496674fc8f
Tara Drwenski <tdrwenski@ucar.edu>
2023-11-30 16:14:30 -0700

    Run caching/non-caching tests against TDS with corresponding config in run all script

--- run-all.sh
+++ run-all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# start tds
+# start tds, no caching config
 cd tds/
 docker build -t thredds-performance-tests:5.5-SNAPSHOT .
 docker compose up -d tds-no-caching-network-host
@@ -11,11 +11,24 @@ until docker inspect --format "{{json .State.Health.Status }}" thredds-performan
 mkdir -p ../tests/version/
 docker cp thredds-performance-tests:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
 
-# run tests
+# run tests for no caching config
 cd ../tests/
 mkdir -p results/
 docker build -t performance-tests:latest .
-docker run --rm --network="host" -v ./results/:/usr/src/app/results/ --user $(id -u):$(id -g) performance-tests
+docker run --rm --network="host" -v ./results/:/usr/tests/results/ --user $(id -u):$(id -g) performance-tests -d /usr/tests/configs/no_caching_tests
+
+# stop tds
+cd ../tds/
+docker compose down
+
+# start tds, caching config
+docker compose up -d tds-caching-network-host
+until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
+| grep -m 1 '"healthy"'; do sleep 1 ; done
+
+# run tests for caching config
+cd ../tests/
+docker run --rm --network="host" -v ./results/:/usr/tests/results/ --user $(id -u):$(id -g) performance-tests -d /usr/tests/configs/caching_tests
 
 # stop tds
 cd ../tds/
