0b8d3c35291f3e467a13f420f9680fa4044b3fd2
Tara Drwenski <tdrwenski@ucar.edu>
2024-03-12 13:18:45 -0600

    Give services unique container_names

--- run-all.sh
+++ run-all.sh
@@ -4,12 +4,12 @@
 cd tds/
 docker build --no-cache -t thredds-performance-tests:5.5-SNAPSHOT .
 docker compose up -d tds-no-caching-network-host
-until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
+until docker inspect --format "{{json .State.Health.Status }}" tds-no-caching-network-host\
 | grep -m 1 '"healthy"'; do sleep 1 ; done
 
 # get tds version info
 mkdir -p ../tests/version/
-docker cp thredds-performance-tests:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
+docker cp tds-no-caching-network-host:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
 
 # run tests for no caching config
 cd ../tests/
@@ -23,7 +23,7 @@ docker compose down
 
 # start tds, caching config
 docker compose up -d tds-caching-network-host
-until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
+until docker inspect --format "{{json .State.Health.Status }}" tds-caching-network-host\
 | grep -m 1 '"healthy"'; do sleep 1 ; done
 
 # run tests for caching config
--- tds/start-no-caching.sh
+++ tds/start-no-caching.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 docker compose up -d tds-no-caching
-until docker inspect --format "{{json .State.Health.Status }}" thredds-performance-tests\
+until docker inspect --format "{{json .State.Health.Status }}" tds-no-caching\
 | grep -m 1 '"healthy"'; do sleep 1 ; done
 
 mkdir -p ../tests/version/
-docker cp thredds-performance-tests:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
+docker cp tds-no-caching:/usr/local/tomcat/webapps/thredds/META-INF/MANIFEST.MF ../tests/version/
